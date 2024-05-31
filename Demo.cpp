06969aba3a22c811ce2db98a23c03d0ad17117f8
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-05-31 03:09:04 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -13,49 +13,64 @@ using namespace ariel;
 
 int main()
 {
-    Player p1("Amit", "\033[31m");
-    Player p2("Yossi", "\033[32m");
-    Player p3("Dana", "\033[0m");
+    string name1,name2,name3;
+    cout << "Welcome to Catan!\n Insert your names" << endl;
+    cout <<"Player 1 (You're red):" << endl;
+    cin >> name1;
+    cout <<"Player 2 (You're blue):" << endl;
+    cin >> name2;
+    cout <<"Player 3 (You're yellow):" << endl;
+    cin >> name3;
+    Player p1(name1, "\033[31m");
+    Player p2(name2, "\033[36m");
+    Player p3(name3, "\033[33m");
     Catan catan(p1, p2, p3);
     // Starting of the game. Every player places two settlements and two roads.
-    catan.ChooseStartingPlayer();   // should print the name of the starting player, assume it is Amit.
-    catan.placeSettelemnt(p1, 8);
-    catan.placeRoad(p1, 8, 13);
-    catan.placeSettelemnt(p1, 14);
-    catan.placeRoad(p1, 9, 14); // p1 chooses Forest, hills, Agricultural Land, Desert with numbers 5, 6, 3, 4.
-    
-    catan.nextTurn();
-
-    catan.placeSettelemnt(p2, 21);
-    catan.placeRoad(p2, 16, 21);
-    try
-    {
-        catan.placeSettelemnt(p3, 8); // p3 tries to place a settlement in the same location as p2.
+    vector<Player> players = catan.ChooseStartingPlayer();   // should print the name of the starting player, assume it is Amit.
+    unsigned int spot1, spot2, choise;
+    for(unsigned int i=0; i<=2; i++){
+        cout <<"Place the 1st Settelment " << players[i].getName() << endl;
+        cin >> spot1;
+        catan.placeSettelemnt(players[i], spot1);
+        cout <<"Place the 1st road " << players[i].getName() << endl;
+        cin >> spot1;
+        cin >> spot2;
+        while(!catan.placeRoad(players[i], spot1, spot2)){
+            cout << "Try again" << endl;
+            cin >> spot1;
+            cin >> spot2;
+        };
+        catan.nextTurn();
     }
-    catch (const std::exception &e)
-    {
-        cout << e.what() << endl;
-    }
-    catan.placeSettelemnt(p2, 17);
-    catan.placeRoad(p2, 16, 22); // p2 chooses Mountains, Pasture Land, and Forest with numbers 4, 9, 5.
-    
     catan.nextTurn();
-
-    catan.placeSettelemnt(p3, 29);
-    catan.placeRoad(p3, 29, 35);
-    catan.placeSettelemnt(p3, 36);
-    catan.placeRoad(p3, 30, 36); // p3 chooses Mountains, Pasture Land, Agricultural Land, Pasture Land with numbers 3, 8, 3, 9.
-
-    // p1 has wood,bricks, and wheat, p2 has wood, ore, and wool, p3 has ore, wool, wheat.
+    for(unsigned int i=2; i>=0; i--){
+        catan.nextTurn();
+        cout <<"Place the 2nd Settelment " << players[i].getName() << endl;
+        cin >> spot1;
+        catan.placeSettelemnt(players[i], spot1);
+        cout <<"Place the 2st road " << players[i].getName() << endl;
+        cin >> spot1;
+        cin >> spot2;
+        while(!catan.placeRoad(players[i], spot1, spot2)){
+            cout << "Try again" << endl;
+            cin >> spot1;
+            cin >> spot2;
+        };
+        catan.nextTurn();
+    }
+    while(!catan.gotWinner()){
+        cout << "It's your turn" << players[catan.getTurn()].getName() << ", chose your move:" << endl;
+        cout << "Build settlement" << endl;
+        cout << "Build road" << endl;
+        cin >> choise;
+        catan.nextTurn();
+    }
     // p1.rollDice();                                    // Lets say it's print 4. Then, p2 gets ore from the mountations.
     // catan.placeRoad(p1, {"Forest", "Hills"}, {5, 6}); // p1 continues to build a road.
-    // p1.endTurn();                                     // p1 ends his turn.
 
     // p2.rollDice(); // Lets say it's print 9. Then, p3 gets wool from the Pasture Land, p2 gets wool from the Pasture Land.
-    // p2.endTurn();  // p2 ends his turn.
 
     // p3.rollDice(); // Lets say it's print 3. Then, p3 gets wheat from the Agricultural Land and Ore from the Mountains, p1 gets wheat from the Agricultural Land.
-    // p3.endTurn();  // p3 ends his turn.
 
     // try
     // {
@@ -68,11 +83,9 @@ int main()
 
     // p1.rollDice();                       // Lets say it's print 6. Then, p1 gets bricks from the hills.
     // p1.trade(p2, "wood", "brick", 1, 1); // p1 trades 1 wood for 1 brick with p2.
-    // p1.endTurn();                        // p1 ends his turn.
 
     // p2.rollDice();           // Lets say it's print 9. Then, p3 gets wool from the Pasture Land, p2 gets wool from the Pasture Land.
     // p2.buyDevelopmentCard(); // p2 buys a development card. Lets say it is a bonus points card.
-    // p2.endTurn();            // p2 ends his turn.
 
     // p1.printPoints(); // p1 has 2 points because it has two settelments.
     // p2.printPoints(); // p2 has 3 points because it has two settelments and a bonus points card.
